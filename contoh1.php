313a1f6950a8cabbeae745951cd90a7f6bea1a04
sufiibadah <145332870+sufiibadah@users.noreply.github.com>
2023-09-25 15:33:52 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,12 @@
+<?php 
+class Contoh1 extends CI_Controller 
+{ 
+    public function index() 
+    { 
+        echo "<h1>Perkenalkan</h1>"; 
+        echo"Nama:  Sufi Ibadah 
+             Tempat Tinggal: Kemanggisan
+             Hobi: olah raga yang saya sukai adalah 
+            Turu"; 
+    } 
+}
